58f9c4d38ccc074c629c810373e6033a5fe20b3f
Lucas Sena <lucasmelosena@gmail.com>
2025-10-08 21:52:16 -0300

    feat: create retailer home with mock data

---
+++ DW1/scripts/retailerHome.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+import { firebaseConfig } from "./config.js";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+        console.log("UID do usuário:", user.uid);
+
+        const userDocRef = doc(db, "users", user.uid);
+        const userDocSnap = await getDoc(userDocRef);
+
+        if (userDocSnap.exists()) {
+            const userData = userDocSnap.data();
+            document.getElementById("retailerName").innerHTML += `${userData.name}!`
+
+            const storeNameElement = document.getElementById('storeNameDisplay');
+            if (storeNameElement) {
+                storeNameElement.textContent = `Bem-vindo à ${userData.storeName}!`;
+            }
+        } else {
+            console.log("Não encontramos dados adicionais para este usuário no Firestore.");
+        }
+
+    } else {
+        console.log("Nenhum usuário logado. Redirecionando...");
+        window.location.href = "index.html";
+    }
+});
